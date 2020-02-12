ab96bd0570112e783fdc80f1deee50d9a3687492
Jeremy Grosser <jeremy@synack.me>
2020-02-12 14:41:27 -0800

    read only rootfs

--- recipes-bsp/u-boot/files/clockwork-cpi3/boot.cmd
+++ recipes-bsp/u-boot/files/clockwork-cpi3/boot.cmd
@@ -5,7 +5,7 @@ if itest.b *0x28 == 0x02 ; then
 	echo "U-boot loaded from eMMC or secondary SD"
 	rootdev=mmcblk1p2
 fi
-setenv bootargs console=tty0 console=${console} root=/dev/${rootdev} rootwait panic=5 vt.color=0xA0 vt.global_cursor_default=0 loglevel=3 ${extra}
+setenv bootargs console=tty0 console=${console} root=/dev/${rootdev} rootwait panic=5 vt.color=0xF0 vt.global_cursor_default=0 loglevel=3 ${extra}
 load mmc 0:1 ${fdt_addr_r} ${fdtfile} || load mmc 0:1 ${fdt_addr_r} boot/${fdtfile}
 load mmc 0:1 ${kernel_addr_r} zImage || load mmc 0:1 ${kernel_addr_r} boot/zImage || load mmc 0:1 ${kernel_addr_r} uImage || load mmc 0:1 ${kernel_addr_r} boot/uImage
 bootz ${kernel_addr_r} - ${fdt_addr_r} || bootm ${kernel_addr_r} - ${fdt_addr_r}
